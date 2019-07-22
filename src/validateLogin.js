3c52bea4c930cfac94390f41972867a19bcfcba9
Rob Levin <roblevintennis@gmail.com>
2019-07-22 16:12:40 -0700

    We seemed to be missing the errors.email for case where it's omitted

--- src/validateLogin.js
+++ src/validateLogin.js
@@ -1,6 +1,7 @@
 export default function validateLogin(values) {
   let errors = {};
   if (!values.email) {
+    errors.email = "Email address is required";
   } else if (!/\S+@\S+\.\S+/.test(values.email)) {
     errors.email = "Email address is invalid";
   }
